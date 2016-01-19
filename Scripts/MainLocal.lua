f7f6cc78148a89b5edcc11f1e94eb18194a5ba35
JaidenK <king.jaiden@yahoo.com>
2016-01-18 21:58:13 -0800

    Camera control

--- Scripts/MainLocal.lua
+++ Scripts/MainLocal.lua
@@ -1,12 +1,44 @@
 local target = workspace.Part
 local camera = workspace.CurrentCamera
-camera.CameraType = Enum.CameraType.Scriptable
+camera.CameraType = Enum.CameraType.Follow
 camera.CameraSubject = target
-local angle = 0
- 
-while wait() do
-   camera.CoordinateFrame = CFrame.new(target.Position)  --Start at the position of the part
-                          * CFrame.Angles(0, angle, 0) --Rotate by the angle
-                          * CFrame.new(0, 0, 5)       --Move the camera backwards 5 units
-   angle = angle + math.rad(1)
+
+local wasd = {0,0,0,0}
+local camVel = CFrame.new(0,0,0)
+local moving = false
+
+
+function onKeyPress(actionName, userInputState, inputObject)
+   print("wpres")
+   if userInputState == Enum.UserInputState.Begin then
+      wasd[1] = 1
+   elseif userInputState == Enum.UserInputState.End then
+      wasd[1] = 0
+   end
+   updateCamVel()
 end
+
+function updateCamVel()
+   if( (wasd[1]==1 and wasd[2]==0 and wasd[3]==0 and wasd[4]==0) )then
+      camVel = CFrame.new(1,0,0) if not moving then moving = not moving move() end
+   else
+      camVel = CFrame.new(0,0,0) 
+      moving = false
+   end
+
+end
+
+function move()
+   repeat 
+      target.CFrame = target.CFrame + camVel
+      wait(0.05)
+   until not moving
+end
+ 
+-- game.ContextActionService:BindAction("keyPress", onKeyPress, false, "Enum.KeyCode.W")
+game.ContextActionService:BindAction("keyPress", onKeyPress, false, Enum.KeyCode.W)
+--game.ContextActionService:BindAction("keyPress", onKeyPress, false, Enum.KeyCode.R)
+--game.ContextActionService:BindAction("keyPress", onKeyPress, false, Enum.KeyCode.R)
+-- The above line could also been written as:
+-- game.ContextActionService:BindAction("keyPress", onKeyPress, false, "r")
+print("Local ready.")
